42ab1073219f234fa18c461485eb7c5aca45b1b6
DepletedNova <40815280+DepletedNova@users.noreply.github.com>
2022-04-16 17:38:30 -0600

    I fix

---
+++ Assets/Scripts/Mutations/Mutator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Mutations
+{
+    public abstract class Mutator
+    {
+        public static List<Type> Mutations = new List<Type>()
+        {
+
+        };
+
+        public enum MutationType
+        {
+            SHIP,
+            WEAPON,
+            PROJECTILE,
+            MISC
+        };
+
+        public abstract MutationType Type { get; }
+
+        public abstract void Init();
+    }
+}
